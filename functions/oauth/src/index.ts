da7b427cc7c810c93dee419493525f92b00e637b
JeongKyungSeo <jerion7474@gmail.com>
2019-04-30 19:49:45 +0900

    Modify function names and roles

--- functions/oauth/src/index.ts
+++
@@ -1,8 +0,0 @@
-import * as random from 'lodash/random';
-
-console.log('starting function');
-
-export default (e, ctx, cb) => {
-  console.log('processing event: %j', e);
-  cb(null, { rand: random(0, 100) });
-};
